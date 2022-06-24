b2b9f5a22d2267bff1fc5d7170197fb593b103b7
afeefahmednccs <108149363+afeefahmednccs@users.noreply.github.com>
2022-06-24 23:45:49 +0500

    Update ai_cust_final.py

--- ai_cust_final.py
+++ ai_cust_final.py
@@ -1,26 +1,28 @@
 from ai_cust_code import final_outcome
 
-
-filename='Intro.wav'
-response=final_outcome(filename)
-if(response=='POSITIVE'):
-    filename = 'Programe Info.wav'
-    response = final_outcome(filename)
-    if (response == 'POSITIVE'):
-        filename = 'Qualification Question.wav'
+def final_function():
+    filename='Intro.wav'
+    response=final_outcome(filename)
+    if(response=='POSITIVE'):
+        filename = 'Programe Info.wav'
         response = final_outcome(filename)
         if (response == 'POSITIVE'):
-            filename = 'Are your Still Making payments on time.wav'
+            filename = 'Qualification Question.wav'
             response = final_outcome(filename)
             if (response == 'POSITIVE'):
-                filename = 'Hold On Let me Trasnfer.wav'
+                filename = 'Are your Still Making payments on time.wav'
                 response = final_outcome(filename)
                 if (response == 'POSITIVE'):
-                    filename = 'Trasnfeing-Confernce.wav'
+                    filename = 'Hold On Let me Trasnfer.wav'
                     response = final_outcome(filename)
                     if (response == 'POSITIVE'):
-                        filename = 'Trasnfer to Agent.wav'
+                        filename = 'Trasnfeing-Confernce.wav'
                         response = final_outcome(filename)
+                        if (response == 'POSITIVE'):
+                            filename = 'Trasnfer to Agent.wav'
+                            response = final_outcome(filename)
+                        else:
+                            print('Sorry, we Dropped your call Thanks.')
                     else:
                         print('Sorry, we Dropped your call Thanks.')
                 else:
@@ -31,13 +33,10 @@ if(response=='POSITIVE'):
             print('Sorry, we Dropped your call Thanks.')
     else:
         print('Sorry, we Dropped your call Thanks.')
-else:
-    print('Sorry, we Dropped your call Thanks.')
 
-
-
-
-
+        
+        
+       
 
 
 
